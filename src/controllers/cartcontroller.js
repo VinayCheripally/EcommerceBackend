539c66080ae99145cb28d3aa25583c807be75e94
VinayCheripally <vinaycheripally1@gmail.com>
2023-08-19 17:51:30 +0530

    add

--- src/controllers/cartcontroller.js
+++ src/controllers/cartcontroller.js
@@ -37,6 +37,44 @@ const cartcontroller = {
         .catch(()=>{
           res.send("error adding into cart");
         })
+    },
+    updateCartItem:(req,res)=>{
+      const user = user.findOne({username:req.body.username})
+        .then(()=>{
+          res.send("found user");
+        })
+        .catch(()=>{
+          res.send("error");
+        })
+
+        cart.find({userId:user._id})
+        .then((cart)=>{
+            cart.items.quantity = req.body.updatedvalue;
+            res.send("updated the quantity");
+        })
+        .catch(()=>{
+          res.send("error updating the quantity");
+        })
+
+    },
+    removeFromCart:(req,res)=>{
+      const user = user.findOne({username:req.body.username})
+        .then(()=>{
+          res.send("found user");
+        })
+        .catch(()=>{
+          res.send("error");
+        })
+        const product = {productId:req.body.productId,quantity:req.body.quantity};
+        cart.find({userId:user._id})
+        .then((cart)=>{
+            cart.items.pop(product);
+            res.send("updated the quantity");
+        })
+        .catch(()=>{
+          res.send("error updating the quantity");
+        })
+
     }
   };
   
